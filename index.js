9fbf200e4317d37740fd9fb239e6cebc7eaa67f1
Bryn <bschulha@ualberta.ca>
2020-09-25 05:06:20 +0000

    Used API requests and callbacks to find ISS flytimes

---
+++ index.js
@@ -0,0 +1,49 @@
+// index.js
+// const { fetchMyIP } = require('./iss');
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+// const { fetchCoordsByIP } = require('./iss');
+
+// fetchCoordsByIP("66.51.135.75", (error, data) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned Coordinates for IP:' , data);
+// });
+
+// const { fetchISSFlyOverTimes } = require('./iss');
+
+// fetchISSFlyOverTimes({ latitude: '49.26400', longitude: '-122.93690' }, (error, data) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned risetimes for IP:' , data);
+// });
+
+const { nextISSTimesForMyLocation } = require('./iss');
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
+});
